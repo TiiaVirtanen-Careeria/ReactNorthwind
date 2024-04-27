0abda722eb70150f82f70e9a2177ea5754b3026f
Tiia Virtanen <tiia.virtanen@student.careeria.fi>
2024-04-27 14:01:32 +0300

    Asiakkaan lisäys valmis

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import React, {useState} from 'react'
 import Laskuri from './Laskuri'
 import Viesti from './Viesti'
 import Posts from './Posts'
+import CustomerList from './CustomerList'
+import Message from './Message'
 
 const App = ()  => {
 
   //App komponentin tila
 const [showLaskuri, setshowLaskuri] = useState(false)
 const [showPosts, setshowPosts] = useState(false)
+// Statet messagen näyttämistä varten
+const [showMessage, setShowMessage] = useState(false)
+const [message, setMessage] = useState('')
+const [isPositive, setIsPositive] = useState(false)
 
 const huomio = () => {
   alert("Huomio!")
@@ -17,6 +23,10 @@ const huomio = () => {
   return (
     <div>
       <h1>Hello from React</h1>
+
+      {showMessage && <Message message={message} isPositive={isPositive} /> }
+
+      <CustomerList setIsPositive={setIsPositive} setMessage={setMessage} setShowMessage={setShowMessage} />
 
       {showPosts && <button onClick={() => setshowPosts(!showPosts)}>Piilota postaukset</button>}
       {!showPosts && <button onClick={() => setshowPosts(!showPosts)}>Näytä postaukset</button>}
